b84e2ed700b5f77430cde0c7cd1dfd6734e1d159
khanh <khanh@KHANHPC>
2026-04-10 22:02:04 -0700

    Add project files.

---
+++ Engine/Game.h
@@ -0,0 +1,69 @@
+/****************************************************************************************** 
+ *	Chili DirectX Framework Version 16.07.20											  *	
+ *	Game.h																				  *
+ *	Copyright 2016 PlanetChili.net <http://www.planetchili.net>							  *
+ *																						  *
+ *	This file is part of The Chili DirectX Framework.									  *
+ *																						  *
+ *	The Chili DirectX Framework is free software: you can redistribute it and/or modify	  *
+ *	it under the terms of the GNU General Public License as published by				  *
+ *	the Free Software Foundation, either version 3 of the License, or					  *
+ *	(at your option) any later version.													  *
+ *																						  *
+ *	The Chili DirectX Framework is distributed in the hope that it will be useful,		  *
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of						  *
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the						  *
+ *	GNU General Public License for more details.										  *
+ *																						  *
+ *	You should have received a copy of the GNU General Public License					  *
+ *	along with The Chili DirectX Framework.  If not, see <http://www.gnu.org/licenses/>.  *
+ ******************************************************************************************/
+#pragma once
+
+#include "Keyboard.h"
+#include "Mouse.h"
+#include "Graphics.h"
+
+class Game
+{
+public:
+	Game( class MainWindow& wnd );
+	Game( const Game& ) = delete;
+	Game& operator=( const Game& ) = delete;
+	void Go();
+private:
+	void ComposeFrame();
+	void UpdateModel();
+	/********************************/
+	/*  User Functions              */
+	void DrawFace(int x, int y);
+	void DrawPoo(int x, int y);
+	void DrawGameOver(int x, int y);
+	void DrawGameTitle(int x, int y);
+	int ClampScreenX(int x, int width);
+	int ClampScreenY(int y, int height);
+	bool IsEaten(int x0, int y0, int width0, int height0, int x1, int y1, int width1, int height1);
+	/********************************/
+private:
+	MainWindow& wnd;
+	Graphics gfx;
+	/********************************/
+	/*  User Variables              */
+	int dudeX = 400;
+	int dudeY = 300;
+	const int dudeWidth = 20;
+	const int dudeHeight = 20;
+	int poo0X = 200;
+	int poo0Y = 400;
+	bool isPoo0Eaten = false;
+	int poo1X = 100;
+	int poo1Y = 200;
+	bool isPoo1Eaten = false;
+	int poo2X = 400;
+	int poo2Y = 200;
+	bool isPoo2Eaten = false;
+	const int pooWidth = 24;
+	const int pooHeight = 24;
+	bool isGameStarted = false;
+	/********************************/
+};
